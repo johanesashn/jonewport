9ea776103048fe63a6ae6c1eb595103ee04432b5
johanesashn <113593955+johanesashn@users.noreply.github.com>
2023-07-28 22:26:20 +0700

    Add files via upload

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ export default function Projects(){
     return (
         <div className="project" id="projects">
             <div className="project-title">
+                <div className="project-label">Cool!</div>
                 <p data-aos="fade-up" data-aos-duration="500" data-aos-delay="0" className="project-title--sub">Latest</p>
                 <h2 data-aos="fade-up" data-aos-duration="500" date-aos-delay="500" className="project-title--main">PROJECTS</h2>
             </div>
