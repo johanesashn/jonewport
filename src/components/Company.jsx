fdd80f6d294f2f99cf180ebf975309db49804209
johanesashn <113593955+johanesashn@users.noreply.github.com>
2023-10-04 22:32:12 +0700

    Add files via upload

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -4,7 +4,7 @@ export default function Company(){
     return (
         <div className="company" id="experience">
             <div className="company-title">
-                <h2 className="company-title--main" data-aos="fade-up" data-aos-duration="500" data-aos-delay="0"><span className="company-label">Outsanding</span> EXPERIENCES</h2>
+                <h2 className="company-title--main" data-aos="fade-up" data-aos-duration="500" data-aos-delay="0"><span className="company-label">Outstanding</span> EXPERIENCES</h2>
                 <p className="company-title--sub" data-aos="fade-up" data-aos-duration="500" data-aos-delay="300">Don't <b>miss</b> the latest happenings in my <b>Porto.</b></p>
             </div>
             <div className="company-cards">
