9ba6b9a5fad7177e22300d1e5d626b07726df77f
johanesashn <113593955+johanesashn@users.noreply.github.com>
2024-04-10 13:14:49 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,20 @@ import Cursor from "./components/Cursor"
 import Skills from "./components/Skills"
 import Company from "./components/Company"
 import Footer from "./components/Footer"
-import { useEffect, useState } from "react"
 
 export default function App(){
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-      setTimeout(() => {
-        setLoading(false)
-      }, 1000);
-      setTimeout(() => {
-        window.scrollTo(0,0)
-      }, 500);
-
-  }, [])
-
   return (
     <main>
       <Cursor/>
-      {loading === true ? 
-        <Loader/> 
-        : 
-        <>
-          <Navbar/>
-          <Jumbotron/>
-          <WalkingText text="STUDENT. WEB-DEVELOPER. FREELANCER. INDONESIAN."/>
-          <Projects/>
-          <Company/>
-          <Skills/>
-          <Footer/>
-          <WalkingText text="GOODBYE. ADIOS. SAYONARA. SELAMAT-TINGGAL." class="footer"/>
-        </>
-      } 
+      <Loader/> 
+      <Navbar/>
+      <Jumbotron/>
+      <WalkingText text="STUDENT. WEB-DEVELOPER. FREELANCER. INDONESIAN."/>
+      <Projects/>
+      <Company/>
+      <Skills/>
+      <Footer/>
+      <WalkingText text="GOODBYE. ADIOS. SAYONARA. SELAMAT-TINGGAL." class="footer"/>
     </main>
   )
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,35 @@
 import { Audio } from  'react-loader-spinner'
+import { useState, useEffect } from 'react';
 
 export default function Loader(){
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+      setTimeout(() => {
+        setLoading(false)
+      }, 1000);
+      setTimeout(() => {
+        window.scrollTo(0,0)
+      }, 500);
+
+  }, [])
+
     return (
-        <div className="loader">
-            <div className="loader-animation">
-                <Audio
-                    color="#222222;"
-                    ariaLabel="audio"
-                    visible={true}
-                />
-            </div>
-            <h3>LOADING</h3>
-        </div>
+        <>
+            {
+                loading
+                &&
+                <div className="loader">
+                    <div className="loader-animation">
+                        <Audio
+                            color="#222222;"
+                            ariaLabel="audio"
+                            visible={true}
+                        />
+                    </div>
+                    <h3>LOADING</h3>
+                </div>
+            }
+        </>
     )
 }
